f546a512c380d48a4da874db22487d3c61a7f861
KacperOrmaniec <kacperormaniec@gmail.com>
2025-06-03 21:34:41 +0200

    Refactor App component layout and update CSS for improved styling and structure

--- SalesTrendAnchor/salestrendanchor.web/src/App.js
+++ SalesTrendAnchor/salestrendanchor.web/src/App.js
@@ -7,9 +7,17 @@ import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      <Button variant="text">Disabled</Button>
+    <div class="flex flex-row bg-gray-100">
+      <div class="flex flex-col items-center space-y-4 bg-white w-56 h-screen border-r-2 border-gray-50">
+        <div>logo</div>
+        <Button variant="text">Dashboard</Button>
+        <Button variant="outlined" color="error">
+          Logout
+        </Button>
+      </div>
+      <div class="flex flex-row justify-left items-center w-screen h-10 bg-white border-b-2 border-gray-50">
+        <div class="">Dashboard</div>
+      </div>
     </div>
   );
 }
