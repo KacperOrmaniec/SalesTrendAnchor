49656a182415d89bd9718e4b04894a346c2ef083
KacperOrmaniec <kacperormaniec@gmail.com>
2025-06-03 20:26:20 +0200

    Add Tailwind CSS for styling and configure project files

--- SalesTrendAnchor/salestrendanchor.web/src/App.js
+++ SalesTrendAnchor/salestrendanchor.web/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Button from "@mui/material/Button";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="text">Disabled</Button>
+    </div>
+  );
 }
 
 export default App;
